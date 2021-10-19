29c344f45169f3dbeddb22c1e85294118e00f6a5
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-10-19 20:17:56 +0800

    Install packages nltk and pattern

--- madlibs.py
+++ madlibs.py
@@ -1,10 +1,18 @@
 
-import re, requests, json
+import nltk
+
+
+nltk.set_proxy('127.0.0.1:41091')
+nltk.download('wordnet')
+
+from uncountable_nouns import uncountable_nouns
+
+from pattern.en import pluralize
 
 # Creating variables
 
 adj1 = input('Type an adjective: ')
-noun1 = input('Type a noun: ')
+noun1 = input('Type a noun (living thing): ')
 verb1 = input('Type a verb: ')
 
 adj2 = input('Type an adjective: ')
@@ -46,7 +54,7 @@ def verb_formatting(verb):
   elif verb == 'have':
     formatted_verb = verb[:-2] + 's'
 
-  elif verb[-1] == 'o' or verb[-1] == 's':
+  elif verb[-1] == 'o' or verb[-1] == 's' or verb[-1] == 'z' or verb[-1] == 'x' or (verb[-1] == 'c' and verb[-1] =='h'):
     formatted_verb = verb + 'es'    
 
   else:
@@ -70,6 +78,9 @@ def formatting_pronoun(noun1):
     
   return pronoun.capitalize()
 
+
+
+
 def verb_infinitive(verb):
 
   """
@@ -90,37 +101,15 @@ def verb_infinitive(verb):
 
   return infinitive_verb
 
+
+
+
 def plural_noun(noun):
-	'''
-	searches Google NGram to see if a word is a countable or not
-	'''
+  if noun in uncountable_nouns:
+    return noun
+  return pluralize(noun)
 
-	# format into url (replace spaces with + for url)
-	thing = re.sub(' ', '\+', noun)
-	url = 'https://books.google.com/ngrams/graph?content=many+' + noun + '%2C+much+' + noun + '&year_start=1800&year_end=2000'
-	response = requests.get(url)
-	html = response.read()
 
-	# extract timeseries data from html source
-	# if an error thrown, it's likely there's no match for the term
-	thing = re.sub('\+', ' ', noun)
-	try:
-		many_data = json.loads(re.search('\{"ngram": "many ' + noun + '".*?\}', html, re.IGNORECASE).group(0))['timeseries']
-		many = sum(many_data) / float(len(many_data))
-	except:
-		many = 0.0
-
-	try:
-		much_data = json.loads(re.search('\{"ngram": "much ' + thing + '".*?\}', html, re.IGNORECASE).group(0))['timeseries']
-		much = sum(much_data) / float(len(much_data))
-	except:
-		much = 0.0
-
-	# return True if countable; False if not
-	if many > much:
-		return noun + 's'
-	return noun
-
-madlib = f'A {adj1} {noun1} {verb_formatting(verb1)} and goes to a {adj2} {noun2}.\n{formatting_pronoun(noun1)} sees {famous_person} {verb_infinitive(verb2)}.\n{noun1.capitalize()}  and {famous_person} decide to {verb3} and have a {color_adjective} {size_adjective} {noun3} together.\n"It will cost an arm and a leg!" - sad {noun1}.\n"Do not worry! I have {plural_noun(noun4)}'
+madlib = f'A {adj1} {noun1} {verb_formatting(verb1)} and goes to a {adj2} {noun2}.\n{formatting_pronoun(noun1)} sees {famous_person} {verb_infinitive(verb2)}.\n{noun1.capitalize()}  and {famous_person} decide to {verb3} and have a {color_adjective} {size_adjective} {noun3} together.\n"It will cost an arm and a leg!" - sad {noun1}.\n"Do not worry! I have a lot of {plural_noun(noun4)}'
 
 print(madlib)
