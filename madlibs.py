e8c0d144c4f6c68a7c2d4f5b3a382b8831c66901
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-10-19 13:16:42 +0800

    Create veriables for input question and madlibs string

---
+++ madlibs.py
@@ -0,0 +1,18 @@
+# Creating variables
+
+adj1 = input('Type an adjective: ')
+noun1 = input('Type a noun: ')
+verb1 = input('Type a verb: ')
+
+adj2 = input('Type an adjective: ')
+noun2 = input('Type a noun: ')
+famous_person = input("Type a famous person: ")
+infinitive_verb = input('Type an infinitive verb: ')
+
+
+
+
+
+madlib = f"A {adj1} {noun1} {verb1} and goes to a {adj2} {noun2}. She sees {famous_person} {infinitive_verb}"
+
+print(madlib)
