f5c465d6192114962e8c19edc590a4e61e2e95e1
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-10-19 14:35:33 +0800

    Add function which transform verb into infinitive verb

--- madlibs.py
+++ madlibs.py
@@ -7,7 +7,7 @@ verb1 = input('Type a verb: ')
 adj2 = input('Type an adjective: ')
 noun2 = input('Type a noun: ')
 famous_person = input("Type a famous person: ")
-infinitive_verb = input('Type an infinitive verb: ')
+verb2 = input('Type a verb: ')
 
 
 dictionary_gender = {
@@ -15,26 +15,33 @@ dictionary_gender = {
   'male': ['man', 'boy', 'babyboy', 'gentleman', ]
 }
 
+dictionary_letters = {
+  'vowels': ['a', 'o', 'i', 'e', 'u'],
+  'consonant': ['b', 'c', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'm', 'n', 'p', 'q', 'r', 's', 't', 'v', 'w', 'x', 'y', 'z']
+}
 
 
-def verb_formatting(verb1):
+
+
+def verb_formatting(verb):
 
   """
   Checks the verb and changes the form of a verb for a sentense
   """
 
-  if verb1[-1] == 'y':
-    formatted_verb = verb1[:-1] + 'ies'
+  if verb[-1] == 'y':
+    formatted_verb = verb[:-1] + 'ies'
 
-  elif verb1[-1] == 'o':
-    formatted_verb = verb1 + 'es'
+  elif verb[-1] == 'o':
+    formatted_verb = verb + 'es'
     
 
   else:
-    formatted_verb = verb1 + 's'
+    formatted_verb = verb + 's'
   return formatted_verb
 
 def formatting_pronoun(noun1):
+
   """
   Checks the noun1 whether it is a female or male noun  
   """
@@ -49,6 +56,22 @@ def formatting_pronoun(noun1):
     
   return pronoun.capitalize()
 
-madlib = f"A {adj1} {noun1} {verb_formatting(verb1)} and goes to a {adj2} {noun2}. {formatting_pronoun(noun1)} sees {famous_person} {infinitive_verb}"
+def verb_infinitive(verb):
+
+  """
+  format verb into infinitive
+  """
+  print(verb)
+  if verb[-2] == 'i' and verb[-1] == 'e':
+    infinitive_verb = verb[:-2] + 'ying'
+  elif  verb[-1] == 'e':
+    infinitive_verb = verb[:-1] + 'ing'
+  elif verb[-2] in dictionary_letters['vowels'] and verb[-1] in dictionary_letters['consonant']:
+    infinitive_verb  = verb + verb[-1] + 'ing'  
+  else:
+    infinitive_verb = verb + 'ing'
+  return infinitive_verb
+
+madlib = f"A {adj1} {noun1} {verb_formatting(verb1)} and goes to a {adj2} {noun2}. {formatting_pronoun(noun1)} sees {famous_person} {verb_infinitive(verb2)}."
 
 print(madlib)
