ee6b37baaf01728547931ec3cb7515c41209ffb4
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-10-19 13:23:34 +0800

    Add formattin verb function

--- madlibs.py
+++ madlibs.py
@@ -11,8 +11,27 @@ infinitive_verb = input('Type an infinitive verb: ')
 
 
 
+def verb_formatting(verb1):
+
+  """
+  Checks the verb and changes the form of a verb for a sentense
+  """
+  
+  if verb1[-1] == 'y':
+    verb1[-1] == 'i'
+    formatted_verb = verb1 + 's'
+    return formatted_verb
+
+  elif verb1[-1] == 'o':
+    formatted_verb = verb1 + 'es'
+    return formatted_verb
+
+  else:
+    formatted_verb = verb1 + 's'
+    return formatted_verb
 
 
-madlib = f"A {adj1} {noun1} {verb1} and goes to a {adj2} {noun2}. She sees {famous_person} {infinitive_verb}"
+
+madlib = f"A {adj1} {noun1} {verb_formatting(verb1)} and goes to a {adj2} {noun2}. She sees {famous_person} {infinitive_verb}"
 
 print(madlib)
