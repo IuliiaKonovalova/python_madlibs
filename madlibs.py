2b3bf1591f1130757666636b63ae69413a949a52
IuliiaKonovalova <juliiakonn@protonmail.com>
2021-10-21 18:09:41 +0800

    Create separate file for gender dictionary

--- madlibs.py
+++ madlibs.py
@@ -11,6 +11,8 @@ import random
 
 from uncountable_nouns import uncountable_nouns
 from idioms import idioms_dictionary
+from gender_words import dictionary_gender
+
 
 
 
@@ -44,10 +46,6 @@ verb4 = input('Type a verb: ')
 
 
 
-dictionary_gender = {
-  'female': ['girl', 'woman', 'girlfriend', 'daughter', 'sister', 'granddaughter', 'lady', 'nun', 'niece', 'madam', 'witch', 'baroness', 'mistress', 'landlady', 'maidservant', 'sultana', 'heroine', 'duchess', 'traitress', 'empress', 'prophetess', 'priestess', 'babygirl', 'mother', 'grandmother', 'aunt', 'wife', 'actress', 'princess', 'waitress', 'queen', 'mailwoman', 'chairwoman', 'policewoman', 'stewardess', 'congresswoman', 'saleswoman'],
-  'male': ['man', 'boy', 'boyfriend', 'son', 'brother', 'grandson' 'babyboy', 'lord', 'monk', 'nephew', 'sir', 'gentleman', 'baron', 'hunter', 'master', 'landlord', 'manservant', 'sultan', 'hero', 'duke', 'traitor', 'emperor', 'shepherd', 'abbot', 'soldier', 'prophet', 'priest', 'wizard', 'father', 'grandfather', 'uncle', 'husband', 'actor', 'prince', 'waiter', 'king', 'mailman', 'chairman', 'policeman', 'steward', 'congressman', 'salesman', '']
-}
 
 dictionary_letters = {
   'vowels': ['a', 'o', 'i', 'e', 'u'],
